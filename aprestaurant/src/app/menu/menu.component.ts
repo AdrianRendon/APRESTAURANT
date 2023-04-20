b4bce630f3a9b020d01e6a494a05a6a29841f0e6
AdrianRendon <adrian.rendon@u-erre.mx>
2023-04-19 18:49:49 -0600

    creacion vista reserrvacion

--- aprestaurant/src/app/menu/menu.component.ts
+++ aprestaurant/src/app/menu/menu.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  menuList = [
+    {
+      title: 'Carne a la parrilla',
+      image: '../../assets/menu/plato1.png',
+      description:
+      'Carne a la parrilla con chile toreado y cebolla asada.'
+    },
+    {
+      title: 'Carne en fajitas',
+      image: '../../assets/menu/plato2.png',
+      description:
+      'Carne a la parrilla en fajitas con chile toreado y cebolla asada.'
+    },
+    {
+      title: 'Todo de la parrilla',
+      image: '../../assets/menu/plato3.png',
+      description:
+      'Carne, cabrito, papa asada, cebolla asada directamente desde la parrilla y una guarnicion ligera de verduras al vapor.'
+    },
+    {
+      title: 'Carne en salsa',
+      image: '../../assets/menu/plato4.png',
+      description:
+      'Carne a la parrilla picada en cuadritos y cocida en un mezcla de chiles, tomate y cebolla.'
+    
+    },
+    {
+      title: 'Regio por primera vez',
+      image: '../../assets/menu/plato5.png',
+      description:
+      'Carne, cabrito, salchicha, cebolla desde la parrilla, un queso flameado con chorizo, guacamole y unas tortillas para armarse unos buenos tacos'
+    },
+    {
+      title: 'El rey del cabrito',
+      image: '../../assets/menu/plato6.png',
+      description:
+      'Cabrito completo pide el corte al gusto.'
+    },
+  ]
 
 }
