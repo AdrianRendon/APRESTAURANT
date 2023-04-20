4cb4fdc89776b00adca7d8f5d60ef92bb194d9d8
AdrianRendon <adrian.rendon@u-erre.mx>
2023-04-19 22:13:39 -0600

    servicios

--- aprestaurant/src/app/menu/menu.component.ts
+++ aprestaurant/src/app/menu/menu.component.ts
@@ -1,50 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Menu } from '../_models/menu.model';
+import { MenuService } from '../_services/menu/menu.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
-  menuList: Menu[] = [
-    {
-      title: 'Carne a la parrilla',
-      image: '../../assets/menu/plato1.png',
-      description:
-      'Carne a la parrilla con chile toreado y cebolla asada.'
-    },
-    {
-      title: 'Carne en fajitas',
-      image: '../../assets/menu/plato2.png',
-      description:
-      'Carne a la parrilla en fajitas con chile toreado y cebolla asada.'
-    },
-    {
-      title: 'Todo de la parrilla',
-      image: '../../assets/menu/plato3.png',
-      description:
-      'Carne, cabrito, papa asada, cebolla asada directamente desde la parrilla y una guarnicion ligera de verduras al vapor.'
-    },
-    {
-      title: 'Carne en salsa',
-      image: '../../assets/menu/plato4.png',
-      description:
-      'Carne a la parrilla picada en cuadritos y cocida en un mezcla de chiles, tomate y cebolla.'
-    
-    },
-    {
-      title: 'Regio por primera vez',
-      image: '../../assets/menu/plato5.png',
-      description:
-      'Carne, cabrito, salchicha, cebolla desde la parrilla, un queso flameado con chorizo, guacamole y unas tortillas para armarse unos buenos tacos'
-    },
-    {
-      title: 'El rey del cabrito',
-      image: '../../assets/menu/plato6.png',
-      description:
-      'Cabrito completo pide el corte al gusto.'
-    },
-  ]
+export class MenuComponent implements OnInit {
+  menuList: Menu[] = [];
+
+  constructor(private menuService: MenuService) {}
+
+  ngOnInit(): void {
+    this.menuList = this.menuService.getMenuList();
+  }
 
 }
--- aprestaurant/src/app/user-list/user-list.component.ts
+++ aprestaurant/src/app/user-list/user-list.component.ts
@@ -1,40 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { User } from '../_models/user.model';
+import { UserService } from '../_services/user/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
-  users: User[] = [
-    {
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },{
-      username: 'adrian.rendon', name: 'Adrian rendon'
-    },
-    
-   ];
+export class UserListComponent implements OnInit {
+    users: User[]= [];
     displayedColumns: string[] = ['username', 'name', 'actions', ];
-  
+     
+    constructor (private userService: UserService) {}
+    ngOnInit(): void {
+      this.users=this.userService.getUsers();
+    }
     addButtonClicked() {
       alert('Boton de adicion activado');
     }
     deleteButtonClick() {
-      alert('Boton de borrar activado');
+      this.userService.deleteUser();
     }
 }
